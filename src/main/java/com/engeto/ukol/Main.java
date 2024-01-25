a4aa684958babb2310e4d11dd318ac49943eb3f0
michal.nespurek <michal.nespurek@email.cz>
2024-01-25 08:33:02 +0100

    ukol1

--- src/main/java/com/engeto/ukol/Main.java
+++ src/main/java/com/engeto/ukol/Main.java
@@ -1,19 +1,81 @@
 package com.engeto.ukol;
 
+import java.math.BigDecimal;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static void ukol10() {
+        System.out.println("Hello world!");
+    }
+    public static void ukol2() {
+        String uzivatel = "Karel";
+        System.out.println(uzivatel);
+    }
+
+    public static void ukol3() {
+        Integer pocetLekci = 10;
+        System.out.println(pocetLekci);
+    }
+
+    public static void ukol4() {
+        double cena = 10.0;
+        System.out.println(cena);
+    }
+
+    public static void ukol5() {
+        BigDecimal cena = BigDecimal.valueOf(0);
+        for (int i = 0; i < 10; i++) {
+            cena = cena.add(BigDecimal.valueOf(0.1));
+        }
+        System.out.println(cena);
+    }
+
+    public static void ukol6() {
+        BigDecimal cena = BigDecimal.valueOf(0);
+        cena = cena.add(BigDecimal.valueOf(0.1));
+        System.out.println(cena);
+    }
+
+    public static void ukol7() {
+        int vykonMotoru = 120;
+        System.out.println("Výkon Motoru je: "+vykonMotoru+" kW.");
+    }
+
+    public static void ukol8() {
+        int velikostKosile = 37;
+        System.out.println("Velikost košile je: "+velikostKosile+".");
+    }
+
+    public static void ukol9() {
+        System.out.println("Metoda *skoro* bez chybičky!");
+    }
+    public static void mrkev() {
+        SalesList salesList = new SalesList();
+        salesList.name = "Michal";
+        salesList.surname = "Nešpůrek";
+        salesList.totalCarrotSold = 10;
+        salesList.deal = 2;
+        int averageSoldCarrot;
+        averageSoldCarrot = salesList.totalCarrotSold/ salesList.deal;
+
+        System.out.println("V průměru se prodalo "+averageSoldCarrot+" tun.");
+
+    }
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("Oprav chyby a spusť všechny metody!");
+        System.out.println("Komentáře odstraníš klávesovou "
+                +"zkratkou <Ctrl>+</> - použij lomítko "
+                +"na numerické klávesnici.");
+        ukol10();
+        ukol2();
+        ukol3();
+        ukol4();
+        ukol5();
+        ukol6();
+        ukol7();
+        ukol8();
+        ukol9();
+        mrkev();
         }
     }
-}
--- src/main/java/com/engeto/ukol/SalesList.java
+++ src/main/java/com/engeto/ukol/SalesList.java
@@ -1,2 +1,16 @@
-package com.engeto.ukol;public class SalesList {
+package com.engeto.ukol;
+import java.time.LocalDate;
+
+public class SalesList {
+    public String name;
+    public String surname;
+    private LocalDate birth;
+    public Integer deal;
+    public Integer totalCarrotSold;
+    private String town;
+    private String licencePlate;
+    private String ip;
+    private Integer fuelConsumption;
+
+
 }
